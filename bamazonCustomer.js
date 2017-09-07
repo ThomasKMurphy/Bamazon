b07935e4cb0eb3d30099420aa892142ba34c2853
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-09-07 10:23:21 -0400

    customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,73 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "Sector9",
-  database: "bamazonDB"
+  user: 'root',
+  password: 'Sector9',
+  database: 'bamazonDB'
 });
+
 connection.connect(function(err) {
   if (err) throw err;
-  // runSearch();
+  console.log('\nConnection Successful!\n');
+  makeTable();
 });
+
+var makeTable = function() {
+  connection.query('SELECT * FROM products', (err, res) => {
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        res[i].item_id + " | " +
+        res[i].product_name + " | " +
+        res[i].department_name + " | " +
+        res[i].price + " | " + 
+        res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  })
+}
+
+var promptCustomer = function(res) {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'choice',
+    message: 'What would you like to purchase?'
+  }]).then(function(answer) {
+    var correct = false;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].product_name === answer.choice) {
+        correct = true;
+        var product = answer.choice;
+        var id = i;
+        inquirer.prompt({
+          type: 'input',
+          name: 'quantity',
+          message: 'How many would you like to buy?',
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }).then(function(answer) {
+          if ((res[id].stock_quantity - answer.quantity) > 0) {
+            connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.quantity) + "' WHERE product_name='" + product + "'", (err, res2) => {
+              console.log('\nProduct Purchased!\n');
+              makeTable();
+            })
+          } else {
+            console.log('Not a valid selection!');
+            promptCustomer(res);
+          }
+        })
+      }
+    }
+    if (i === res.length && correct === false) {
+      console.log('Not a valid selection!');
+      promptCustomer(res);
+    }
+  })
+}
--- schema.sql
+++ schema.sql
@@ -13,13 +13,22 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES (),
-VALUES ();
+VALUES (101, "Playstation 4", "Entertainment", 300.00, 9);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (102, "Yoga Mat", "Fitness", 25.00, 5);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (103, "Sunglasses", "Fashion", 150.00, 7);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (104, "Protein Powder", "Food", 35.00, 6);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (105, "Dog Food", "Pets", 12.00, 11);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (106, "Skateboard", "Fitness", 125.00, 4);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (107, "Keyboard", "Technology", 100.00, 8);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (108, "Shovel", "Outdoor", 19.00, 2);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (109, "Axe", "Outdoor", 22.00, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (110, "Salt", "Food", 3.00, 41);
